4d4c5807c1c5344359ecb8ff4ff73009eaccee3b
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-04-02 17:00:59 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    const sentence = document.getElementById('text')
+    sentence.textContent = "This is really cool!"
+})
